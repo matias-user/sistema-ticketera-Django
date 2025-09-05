a7fa76c2275a7a366d045c401947efab7be3461a
matias <matias.abarca2018@gmail.com>
2025-09-05 18:37:24 -0400

    Cambio en settings.py se agrega app catalog

--- app/project_ticket/settings.py
+++ app/project_ticket/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticket'
+    'ticket',
+    'catalog'
 ]
 
 MIDDLEWARE = [
