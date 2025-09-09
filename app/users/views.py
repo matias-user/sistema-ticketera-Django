40ec532ca38a390c7f132d84cfcb11c292048341
matias <matias.abarca2018@gmail.com>
2025-09-09 14:33:20 -0300

    Se agrega protección en vista profile de usuario

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 from users.models import Profile
 
 # Create your views here.
-class ProfileDetailView(DetailView):
+class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'users/profile.html'
+    login_url = reverse_lazy('users:login')
