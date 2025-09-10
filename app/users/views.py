350cdbdfa1530ce1b5aaf9a7d7ed0fb2fd718fbd
matias <matias.abarca2018@gmail.com>
2025-09-10 17:29:43 -0300

    Se arregla navbar nachor Perfil, se arregla el argumento del id del profile, y se agrega if en template de profile ya que hasta ahora se mostrarán allí los tickets cerrados

--- app/users/views.py
+++ app/users/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 
 from users.models import Profile
+from ticket.models import Ticket
 
 # Create your views here.
 class ProfileDetailView(DetailView):
     model = Profile
     template_name = 'users/profile.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context['tickets'] = Ticket.objects.filter(user=profile.user)
+        return context
