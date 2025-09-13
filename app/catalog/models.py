2e19eba08c46b5d85525189c0604fe1116ff1ff4
matias <matias.abarca2018@gmail.com>
2025-09-13 19:19:40 -0300

    Se arreglan en algunos modelos el campo description se agrega el default= y se mejora vista de detail_ticket

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=100)
-    description = models.CharField(max_length=200, null=True, blank=True)
+    description = models.CharField(max_length=200, default="", blank=True)
 
     def __str__(self) -> str:
         return self.name
@@ -17,14 +17,14 @@ class State(models.Model):
 
 class Priority(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=100)
-    description = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self) -> str:
         return self.name
 
 class Department(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=100)
-    description = models.CharField(max_length=200, null=True, blank=True)
+    description = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self) -> str:
         return self.name
